6cd6dee3b31c2958f8564ac7f8bb73f177a0f897
imjchiang <jchiang296@gmail.com>
2020-08-28 10:59:40 -0700

    [procedures, contact] add components

---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Contact extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Contact Us</h1>
+                <p>Josh Chiang, MD</p>
+                <p>Email: josh@dentist.com</p>
+                <p>Phone: 123-456-7890</p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
